ef33f161f0c46b10e19e425c1470844d32617100
Dluck13 <dimitri.luck@revature.net>
2022-05-11 19:34:25 -0400

    cross-origin

--- src/main/java/com/dima/online/shopping/repository/BookRepository.java
+++ src/main/java/com/dima/online/shopping/repository/BookRepository.java
@@ -2,11 +2,13 @@ package com.dima.online.shopping.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dima.online.shopping.entity.Book;
 
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
